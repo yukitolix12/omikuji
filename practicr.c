fcc8a4df99d02eac8744fe22f700cba10eae42e3
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-03 14:14:45 +0900

    乱数設定

--- practicr.c
+++ practicr.c
@@ -12,11 +12,11 @@ printf("今日の運勢は・・・\n");
 srand((unsigned)time(NULL));
 fortune_slip = rand() % 4;
 
-if(fortune_slip == 0){
+if(fortune_slip >= 0 && fortune_slip <= 4){
     printf("\nおみくじの結果: [大吉]です!!\n");
-}else if(fortune_slip == 1){
+}else if(fortune_slip >= 5 && fortune_slip <= 6){
     printf("\nおみくじの結果: [中吉]です!!\n");
-}else if(fortune_slip == 2){
+}else if(fortune_slip >= 7 && fortune_slip <= 8){
     printf("\nおみくじの結果: [小吉]です!!\n");
 }else{
     printf("\nおみくじの結果: [吉]です!!\n");

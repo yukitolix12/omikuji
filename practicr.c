333099fda7c4ac0a58193e5e5af28260fa5a87c5
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-03 14:06:00 +0900

    if文作成

--- practicr.c
+++ practicr.c
@@ -9,4 +9,17 @@ printf("おみくじを引こう!([enter]キーを押してね)\n");
 getchar();
 printf("今日の運勢は・・・\n");
 
+srand((unsigned)time(NULL));
+fortune_slip = rand() % 4;
+
+if(fortune_slip == 0){
+    printf("\nおみくじの結果: [大吉]です!!\n");
+}else if(fortune_slip == 1){
+    printf("\nおみくじの結果: [中吉]です!!\n");
+}else if(fortune_slip == 2){
+    printf("\nおみくじの結果: [小吉]です!!\n");
+}else{
+    printf("\nおみくじの結果: [吉]です!!\n");
 }
+}
+
